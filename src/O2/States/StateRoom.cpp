c157a906a37b43c375e17f7a91186df4565ac975
SirusDoma <com@cxo2.me>
2021-03-23 01:21:03 +0700

    Initial StateRoom implementation

--- src/O2/States/StateRoom.cpp
+++ src/O2/States/StateRoom.cpp
@@ -12,6 +12,12 @@ void StateRoom::Initialize()
     auto background = Gx::ResourceManager::Instance()->Create<Gx::Sprite>("Metadata\\State\\Room\\Background.json");
     AddChild(background);
 
+    auto btnCreateRoom = Gx::ResourceManager::Instance()->Create<Gx::Button>("Metadata\\State\\Room\\Btn_CreateRoom.json");
+    AddChild(btnCreateRoom);
+
+    auto btnBack = Gx::ResourceManager::Instance()->Create<Gx::Button>("Metadata\\State\\Room\\Btn_Back.json");
+    AddChild(btnBack);
+
     m_bgm = Gx::ResourceManager::Instance()->Create<sf::Music>("Metadata\\State\\Room\\Music.json");
     m_bgm->play();
 }
