e174828a541c6948ed7ab6ffb33680a19e42e392
CXO2 <com@cxo2.me>
2021-04-10 15:00:07 +0700

    Add missing header

---
+++ include/O2/States/Components/Room/RoomList.hpp
@@ -0,0 +1,10 @@
+#ifndef O2JAM_ROOM_ROOMLIST_HPP
+#define O2JAM_ROOM_ROOMLIST_HPP
+
+#include <Genode/UI/UiContainer.hpp>
+
+class RoomList : public Gx::UiContainer
+{
+};
+
+#endif
---
+++ include/O2/States/Components/Room/UserList.hpp
@@ -0,0 +1,12 @@
+#ifndef O2JAM_ROOM_USERLIST_HPP
+#define O2JAM_ROOM_USERLIST_HPP
+
+#include <Genode/UI/UiContainer.hpp>
+
+class UserList : public Gx::UiContainer
+{
+    UserList();
+    void Initialize();
+};
+
+#endif
