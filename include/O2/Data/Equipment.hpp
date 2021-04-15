d1094aa0f90af07ee43bfdde8a04513829ecaad7
CXO2 <com@cxo2.me>
2021-04-16 01:51:08 +0700

    Fix Equipment order

--- include/O2/Data/Equipment.hpp
+++ include/O2/Data/Equipment.hpp
@@ -45,10 +45,10 @@ namespace Equipment
     enum class Instrument
     {
         None,
-        Piano,
         Bass,
-        Drum,
-        Guitar
+        Guitar,
+        Piano,
+        Drum
     };
 }
 
