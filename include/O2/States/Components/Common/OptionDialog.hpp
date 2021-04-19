4e0712990bbed8a8f400ff3dd926ebce1171f2ac
CXO2 <com@cxo2.me>
2021-04-19 14:07:28 +0700

    Update OptionDialog header

--- include/O2/States/Components/Common/OptionDialog.hpp
+++ include/O2/States/Components/Common/OptionDialog.hpp
@@ -4,6 +4,9 @@
 #include <Genode/UI/Dialog.hpp>
 #include <Genode/UI/Image.hpp>
 
+#include <O2/Chart/EventChannel.hpp>
+#include <O2/Config/GameConfig.hpp>
+
 class OptionDialog : public Gx::Dialog
 {
 public:
@@ -12,8 +15,18 @@ public:
     void Initialize(Gx::Scene &scene);
 
 private:
-    Gx::Image *m_background;
+    virtual void Update(double delta);
+    virtual void OnKeyDown(sf::Event::KeyEvent ev);
+    virtual void OnKeyType(sf::Event::TextEvent ev);
+
+    Gx::Image *m_background, *m_keySelect;
     Gx::UiContainer m_keyOptionContainer, m_soundOptionContainer;
+    std::map<EventChannel, Gx::Image*> m_keyTexts;
+
+    GameConfig   m_config;
+    EventChannel m_keyChannel;
+
+    bool m_keyTestEnabled;
 };
 
 #endif
