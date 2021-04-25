48db221860d10335b38674c429e323d80742661f
CXO2 <com@cxo2.me>
2021-04-25 14:50:22 +0700

    Add Back button to StateWaiting

--- src/O2/States/StateWaiting7K.cpp
+++ src/O2/States/StateWaiting7K.cpp
@@ -1,4 +1,9 @@
 #include <O2/States/StateWaiting7K.hpp>
+
+#include <Genode/UI/Button.hpp>
+
+#include <O2/States/StatePlanet.hpp>
+#include <O2/States/StateRoom.hpp>
 #include <O2/Data/Room.hpp>
 
 #include <O2/Character/Avatar.hpp>
@@ -14,6 +19,15 @@ void StateWaiting7K::Initialize()
 
     auto background = Create<Gx::Sprite>("Interface/Metadata/State/Waiting/Background.json");
     AddChild(background);
+
+    auto btnBack = Create<Gx::Button>("Interface/Metadata/State/Waiting/Btn_Back.json");
+    btnBack->SetClickCallback([&] (auto& sender, auto& ev)
+    {
+        mixer.StopAll();
+        QueueSceneEvent([=] () { GetDirector().SetScene(new StateRoom(Planet::MusicHall::Kaliope, Planet::ChannelInfo{1})); });
+    });
+
+    AddChild(btnBack);
 
     auto player = Room::PlayerInfo{1, 100, "CXO2", false, Character::Gender::Male};
     auto avatar = Create<Avatar>("Interface/Metadata/State/Waiting/Avatar.json");
