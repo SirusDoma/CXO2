e7c876d4fbb64a3d55bd838ce43e079ab2b7faad
CXO2 <com@cxo2.me>
2024-03-28 11:08:36 +0700

    Avoid updating working directory when debugger is attached

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,16 @@
 
 #include <boxer/boxer.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #ifdef __APPLE__
 #include <CoreFoundation/CoreFoundation.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
 
 std::string GetAppBundlePath()
 {
@@ -30,22 +38,67 @@ std::string GetAppBundlePath()
 }
 #endif
 
+#ifdef __linux__
+#include <unistd.h>
+#include <sys/prctl.h>
+#include <signal.h>
+#endif
+
+bool IsDebuggerAttached()
+{
+#ifdef _WIN32
+    return IsDebuggerPresent();
+#endif
+
+#ifdef __APPLE__
+    int mib[4];
+    struct kinfo_proc info{};
+    size_t size;
+
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PROC_PID;
+    mib[3] = getpid();
+
+    size = sizeof(info);
+    if (sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, nullptr, 0) == -1) {
+        return false;
+    }
+
+    return (info.kp_proc.p_flag & P_TRACED) != 0;
+#endif
+
+#ifdef __linux__
+    // Check if the process is being traced by a debugger
+    return prctl(PR_GET_DUMPABLE, 0, 0, 0, 0) != 0;
+#endif
+
+    // Unsupported platform
+    return false;
+}
+
 int main(int argc , char** argv)
 {
     try
     {
         // Fix weird working directory in macOS
-        std::string path = std::string(argv[0]);
+        if (!IsDebuggerAttached())
+        {
+            std::string path = std::string(argv[0]);
 #ifdef __APPLE__
-        path = GetAppBundlePath();
+            path = GetAppBundlePath();
 #endif
-        Gx::LocalFileSystem::SetWorkingDirectory(path);
+            Gx::LocalFileSystem::SetWorkingDirectory(path);
+        }
 
         auto o2jam = O2Jam("O2-JAM", sf::VideoMode(800, 600), sf::VideoMode(800, 600));
         return o2jam.Start();
     }
     catch (std::exception &ex)
     {
+        if (IsDebuggerAttached())
+            throw ex;
+
         auto details = std::string();
         if (typeid(ex) == typeid(Gx::ResourceAccessException) || typeid(ex) == typeid(Gx::ResourceLoadException))
         {
