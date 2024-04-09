bf87a9fcd643826d83345de05d0d6c8d72beb193
CXO2 <com@cxo2.me>
2024-04-10 00:00:56 +0700

    SpriteMetadata missing adjustment

--- include/OTwo/Metadata/Graphics/SpriteMetadata.hpp
+++ include/OTwo/Metadata/Graphics/SpriteMetadata.hpp
@@ -1,17 +1,21 @@
 #ifndef O2JAM_SPRITE_METADATA_HPP
 #define O2JAM_SPRITE_METADATA_HPP
 
+#include <OTwo/Metadata/Graphics/TransformMetadata.hpp>
+
+#include <Genode/Graphics/BlendMode.hpp>
+
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/Rect.hpp>
 
-#include <OTwo/Metadata/Graphics/TransformMetadata.hpp>
 
 struct SpriteMetadata : TransformMetadata
 {
     ~SpriteMetadata() override = default;
 
-    sf::Color   Color;
-    sf::IntRect TexCoords;
+    sf::Color     Color;
+    sf::IntRect   TexCoords;
+    Gx::BlendMode BlendMode;
 };
 
 #endif
