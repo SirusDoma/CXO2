5b6d0862cd5242894e6be8bbb7d50798f6f98c54
CXO2 <com@cxo2.me>
2024-09-26 13:34:34 +0900

    CartContext `Add` and `Remove` now return `bool`

--- include/OTwo/Contexts/CartContext.hpp
+++ include/OTwo/Contexts/CartContext.hpp
@@ -9,11 +9,11 @@ class CartContext : public Serializable<Mx::Cart>
 public:
     CartContext() = default;
 
-    void AddEquipment(unsigned int id);
-    void AddMusic(unsigned int id);
-    void AddEquipmentSet(unsigned int id);
+    bool AddEquipment(unsigned int id);
+    bool AddMusic(unsigned int id);
+    bool AddEquipmentSet(unsigned int id);
 
-    void Remove(std::size_t index);
+    bool Remove(std::size_t index);
 
     bool Contains(Mx::CartItemType type, unsigned int id);
     const std::vector<Mx::CartItem>& GetItems() const;
--- src/OTwo/Contexts/CartContext.cpp
+++ src/OTwo/Contexts/CartContext.cpp
@@ -1,35 +1,39 @@
 #include <OTwo/Contexts/CartContext.hpp>
 
-void CartContext::AddEquipment(const unsigned int id)
+bool CartContext::AddEquipment(const unsigned int id)
 {
     if (Contains(Mx::CartItemType::Equipment, id))
-        return;
+        return false;
 
     Record.Items.push_back({id, Mx::CartItemType::Equipment });
+    return true;
 }
 
-void CartContext::AddMusic(const unsigned int id)
+bool CartContext::AddMusic(const unsigned int id)
 {
     if (Contains(Mx::CartItemType::Music, id))
-        return;
+        return false;
 
     Record.Items.push_back({id, Mx::CartItemType::Music });
+    return true;
 }
 
-void CartContext::AddEquipmentSet(const unsigned int id)
+bool CartContext::AddEquipmentSet(const unsigned int id)
 {
     if (Contains(Mx::CartItemType::EquipmentSet, id))
-        return;
+        return false;
 
     Record.Items.push_back({id, Mx::CartItemType::EquipmentSet });
+    return true;
 }
 
-void CartContext::Remove(const std::size_t index)
+bool CartContext::Remove(const std::size_t index)
 {
     if (index >= Record.Items.size())
-        return;
+        return false;
 
     Record.Items.erase(Record.Items.begin() + index);
+    return true;
 }
 
 const std::vector<Mx::CartItem>& CartContext::GetItems() const
