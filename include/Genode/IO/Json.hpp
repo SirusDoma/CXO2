39c2f65965797e9bfea9074ee2732bece21e7dc2
CXO2 <com@cxo2.me>
2024-10-09 02:01:25 +0900

    Add `std::optional` ADL Serializer

--- include/Genode/IO/Json.hpp
+++ include/Genode/IO/Json.hpp
@@ -2,7 +2,29 @@
 #define GENODE_IO_JSON_HPP
 
 #include <string>
+#include <optional>
+
 #include <nlohmann/json.hpp>
+
+template <typename T>
+struct nlohmann::adl_serializer<std::optional<T>>
+{
+    static void to_json(json& j, const std::optional<T>& opt)
+    {
+        if (opt == std::nullopt)
+            j = nullptr;
+        else
+            j = *opt;
+    }
+
+    static void from_json(const json& j, std::optional<T>& opt)
+    {
+        if (j.is_null())
+            opt = std::nullopt;
+        else
+            opt = j.get<T>();
+    }
+};
 
 namespace Gx
 {
