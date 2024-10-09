26772d4271580e8b531899c42bfb5881f4ac1194
CXO2 <com@cxo2.me>
2024-10-09 23:12:28 +0900

    Revamp Application ADL Serializer
    - Add support for map with numeric key
    - Generalize enum serializer

--- include/OTwo/IO/AdlSerializer.hpp
+++ include/OTwo/IO/AdlSerializer.hpp
@@ -6,35 +6,71 @@
 
 #include <magic_enum.hpp>
 
-template<>
-struct nlohmann::adl_serializer<Gender>
+template<typename T, typename = void>
+struct JsonKeyConverter
 {
-    static void from_json(const nlohmann::json& j, Gender& p)
+    static bool FromString(const std::string& key, T& value);
+};
+
+template<typename Numeric>
+struct JsonKeyConverter<Numeric, std::enable_if_t<std::is_integral_v<Numeric>>>
+{
+    static bool FromString(const std::string& key, Numeric& value)
     {
-        if (const auto parsed = magic_enum::enum_cast<Gender>(j.get<std::string>()); parsed.has_value())
-            p = parsed.value();
+        value = static_cast<Numeric>(std::stoi(key));
+        return true;
     }
 };
 
-template<>
-struct nlohmann::adl_serializer<Planet>
+template<typename Enum>
+struct JsonKeyConverter<Enum, std::enable_if_t<std::is_enum_v<Enum>>>
 {
-    static void from_json(const nlohmann::json& j, Planet& p)
+    static bool FromString(const std::string& key, Enum& value)
     {
-        if (const auto parsed = magic_enum::enum_cast<Planet>(j.get<std::string>()); parsed.has_value())
-            p = parsed.value();
+        const auto parsed = magic_enum::enum_cast<Enum>(key, magic_enum::case_insensitive);
+        value = parsed.value_or(Enum());
+
+        return parsed.has_value();
     }
 };
 
-template<>
-struct nlohmann::adl_serializer<Currency>
+template <typename K, typename V>
+struct nlohmann::adl_serializer<std::map<K, V>>
 {
-    static void from_json(const nlohmann::json& j, Currency& p)
+    static void from_json(const json& j, std::map<K, V>& map)
     {
-        if (const auto parsed = magic_enum::enum_cast<Currency>(j.get<std::string>()); parsed.has_value())
-            p = parsed.value();
+        for (const auto& e : j.items())
+        {
+            auto key = K();
+            if (JsonKeyConverter<K>::FromString(e.key(), key))
+                map[key] = e.value().get<V>();
+        }
     }
 };
 
+template <typename K, typename V>
+struct nlohmann::adl_serializer<std::unordered_map<K, V>>
+{
+    static void from_json(const json& j, std::unordered_map<K, V>& map)
+    {
+        for (const auto& e : j.items())
+        {
+            auto key = K();
+            if (JsonKeyConverter<K>::FromString(e.key(), key))
+                map[key] = e.value().get<V>();
+        }
+    }
+};
+
+
+template<typename Enum>
+struct nlohmann::adl_serializer<Enum, std::enable_if_t<std::is_enum_v<Enum>>>
+{
+    static void from_json(const json& j, Enum& p)
+    {
+        if (const auto parsed = magic_enum::enum_cast<Enum>(j.get<std::string>(), magic_enum::case_insensitive); parsed.has_value())
+            p = parsed.value();
+    }
+};
 
 #endif
