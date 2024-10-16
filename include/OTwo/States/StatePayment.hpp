d7e601d828cef943d7e53e21fcd3bd9244222f64
CXO2 <com@cxo2.me>
2024-10-17 02:15:02 +0900

    Add Gem and Cash numbers in StatePayment

--- include/OTwo/States/StatePayment.hpp
+++ include/OTwo/States/StatePayment.hpp
@@ -4,14 +4,16 @@
 #include <Genode/SceneGraph.hpp>
 #include <OTwo/States/State.hpp>
 
+class SessionContext;
 class StatePayment : public State
 {
 public:
-    explicit StatePayment(Gx::Mixer& mixer);
+    explicit StatePayment(Gx::Mixer& mixer, SessionContext& session);
     void Initialize() override;
 
 private:
     Gx::Mixer& m_mixer;
+    SessionContext& m_session;
 };
 
 #endif
--- src/OTwo/States/StatePayment.cpp
+++ src/OTwo/States/StatePayment.cpp
@@ -1,18 +1,29 @@
 ﻿#include <OTwo/States/StatePayment.hpp>
 #include <OTwo/States/StateItemShop.hpp>
+#include <OTwo/Contexts/SessionContext.hpp>
 
 #include <Genode/UI/Button.hpp>
+#include <Genode/UI/BitmapNumber.hpp>
 
 #include <SFML/Audio/Music.hpp>
 
-StatePayment::StatePayment(Gx::Mixer& mixer) :
-    m_mixer(mixer)
+StatePayment::StatePayment(Gx::Mixer& mixer, SessionContext& session) :
+    m_mixer(mixer),
+    m_session(session)
 {
 }
 
 void StatePayment::Initialize()
 {
     State::Initialize();
+
+    const auto& player = m_session.GetCurrentPlayer();
+
+    const auto currentGem = Instantiate<Gx::BitmapNumber>("IDC_NUMBER_GEM");
+    currentGem->SetValue(player.Gem);
+
+    const auto currentCash = Instantiate<Gx::BitmapNumber>("IDC_NUMBER_CASH");
+    currentCash->SetValue(player.Cash);
 
     const auto backButton = Instantiate<Gx::Button>("IDC_BUTTON_BACK");
     backButton->SetClickCallback([this] (auto&, auto&)
