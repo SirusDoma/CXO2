e4ebfbd32afe67ac5b57adc8100ff6bc8bbc95d8
CXO2 <com@cxo2.me>
2025-06-29 09:18:51 +0900

    Improve debugger utilities
    Signed-off-by: CXO2 <com@cxo2.me>

--- include/Genode/Utilities/Debugger.hpp
+++ include/Genode/Utilities/Debugger.hpp
@@ -18,8 +18,35 @@
 #include <signal.h>
 #endif
 
+#ifndef NDEBUG
+#include <livepp/API/x64/LPP_API_x64_CPP.h>
+#endif
+
 namespace Gx
 {
+    class DebuggerWatcher
+    {
+    public:
+        DebuggerWatcher() {}
+
+        // ReSharper disable once CppNonExplicitConvertingConstructor
+        DebuggerWatcher(const std::any& agent) : m_agent(agent) {}
+
+        ~DebuggerWatcher()
+        {
+#ifndef NDEBUG
+            if (m_agent.has_value())
+            {
+                const auto& agent = std::any_cast<lpp::LppDefaultAgent>(m_agent);
+                lpp::LppDestroyDefaultAgent(const_cast<lpp::LppDefaultAgent*>(&agent));
+            }
+#endif
+        }
+
+    private:
+        std::any m_agent;
+    };
+
     class Debugger
     {
     public:
@@ -55,6 +82,17 @@ namespace Gx
 
             // Unsupported platform
             return false;
+        }
+
+        static void Watch()
+        {
+            const auto agent = lpp::LppCreateDefaultAgent(nullptr,  L"extlibs/libs/internal/livepp");
+
+            if (!lpp::LppIsValidDefaultAgent(&agent))
+                return;
+
+            agent.EnableModule(lpp::LppGetCurrentModulePath(), lpp::LPP_MODULES_OPTION_ALL_IMPORT_MODULES, nullptr, nullptr);
+            static DebuggerWatcher watcher = { agent };
         }
     };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,12 @@
 #include <OTwo/O2Jam.hpp>
 #include <OTwo/Utilities/NamedMutex.hpp>
+#include <OTwo/Utilities/SystemMessageBox.hpp>
 
 #include <Genode/IO/FileSystem/LocalFileSystem.hpp>
 #include <Genode/System/Exception.hpp>
 #include <Genode/IO/IOException.hpp>
 #include <Genode/Utilities/Debugger.hpp>
 #include <Genode/Utilities/StringHelper.hpp>
-
-#ifdef USE_BOXER
-#include <boxer/boxer.h>
-#else
-#include <messagebox-x11/messagebox.h>
-#endif
 
 #include <iostream>
 #include <exception>
@@ -29,21 +24,18 @@ int main(int argc , char** argv)
             // Disabled when debugger is attached for developer & modder convenient
             if (!mutex.IsLocked())
             {
-#ifdef USE_BOXER
-                boxer::show("O2JAM is already running.", "[INFO]");
-#else
-                wchar_t text[] = L"OK";
-                const std::vector<Button> buttons = { { text } };
-
-                Messagebox("[INFO]", "O2JAM is already running.", buttons.data(), 1);
-#endif
-
+                SystemMessageBox::ShowInformation("O2JAM is already running.", "[INFO]");
                 return 1;
             }
 
             // "Fix" macOS translocation
             if (const auto path = Gx::LocalFileSystem::GetApplicationDirectoryPath(); !path.empty())
                 Gx::LocalFileSystem::SetWorkingDirectory(path);
+        }
+        else
+        {
+            // Enable Live++ when debugger attached (optional)
+            Gx::Debugger::Watch();
         }
 
         auto o2jam = O2Jam("O2-JAM", sf::VideoMode({800, 600}), sf::View({400, 300}, {800, 600}), true);
@@ -72,15 +64,7 @@ int main(int argc , char** argv)
         if (Gx::Debugger::IsDebuggerAttached())
             message = "[" + Gx::StringHelper::GetTypeName(ex).toAnsiString() + "]\n" + message;
 
-#ifdef USE_BOXER
-        boxer::show(message.c_str(), "Fatal Error", boxer::Style::Error);
-#else
-        wchar_t text[] = L"OK";
-        const std::vector<Button> buttons = { { text } };
-
-        Messagebox("Fatal Error", std::wstring(message.begin(), message.end()).c_str(), buttons.data(), 1);
-#endif
-
+        SystemMessageBox::ShowError(message, "Fatal Error");
         return -1;
     }
 }
