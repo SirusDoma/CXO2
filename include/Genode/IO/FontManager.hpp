20b29d66bae9f3541922b42e80f1019c744d2d67
CXO2 <com@cxo2.me>
2026-06-30 01:36:01 +0900

    Refactor FontManager

--- include/Genode/IO/FontManager.hpp
+++ include/Genode/IO/FontManager.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
+#include <Genode/IO/ResourceContainer.hpp>
+
 #include <SFML/Graphics/Font.hpp>
 
 #include <cstdint>
-#include <filesystem>
 #include <memory>
 #include <optional>
 #include <string>
-#include <string_view>
-#include <unordered_map>
+#include <utility>
 #include <vector>
 #include <mutex>
 
@@ -25,23 +25,12 @@ namespace Gx
         [[nodiscard]] std::optional<std::pair<const void*, std::size_t>> GetData(const std::string& key);
         [[nodiscard]] std::optional<std::pair<const void*, std::size_t>> GetDefaultData();
 
-        [[nodiscard]] std::vector<std::string> GetKeys() const;
-
         bool Store(const std::string& key, std::vector<std::uint8_t> bytes);
 
         void Clear();
 
     private:
-        [[nodiscard]] static std::optional<std::string> ResolveFontPath(const std::string& nameOrPath);
-        [[nodiscard]] static std::optional<std::string> ResolveDefaultFontPath();
-
-        [[nodiscard]] static std::optional<std::vector<std::uint8_t>> ReadAllBytes(const std::string& filePath);
-
-        [[nodiscard]] static std::string NormalizeKey(std::string_view nameOrPath);
-
         mutable std::mutex m_mutex;
-        std::unordered_map<std::string, std::vector<std::uint8_t>> m_cache;
+        ResourceContainer<std::vector<std::uint8_t>> m_cache;
     };
 }
-
-
